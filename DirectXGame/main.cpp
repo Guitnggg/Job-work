7614574ea2e599647b3fac88684c9a3ea0f54c14
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2025-07-08 14:34:14 +0900

    一番最初

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,54 @@
 #include <Windows.h>
+#include <KamataEngine.h>
+
+#include "GameScene.h"
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
-	return 0;
+
+    // KamataEngineの初期化
+    KamataEngine::Initialize(L"LE3C_14_タカキ_ケンゴ");
+
+    // DirectXCommonのインスタンスの取得
+    DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+    //===============
+    // Mainループ
+    //===============
+    while (true) {
+
+        // エンジンの更新
+        if (KamataEngine::Update()) {
+            break;
+        }
+
+        //=============
+        // 更新処理
+        //=============
+
+        
+
+        //=============
+        /// 描画処理 
+        //=============
+
+        // 描画処理開始       
+        dxCommon->PreDraw();
+
+       
+
+        //描画処理終了       
+        dxCommon->PostDraw();
+
+
+    }
+
+   
+
+    // KamataEngineの終了 
+    KamataEngine::Finalize();
+
+    return 0;
 }
