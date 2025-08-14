1eb2415e88ef569c9982350ef437c5feb6e07ca2
k023g0119 <k023g0119@g.neec.ac.jp>
2025-08-15 01:58:10 +0900

    あ

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -41,17 +41,7 @@ void GameScene::Update() {
 
     skydome_->Update();
 
-    player_->Update();
-
-
-
-
-
-
-
-
-
-    
+    player_->Update(); 
 
     //if (input_->PushKey(DIK_SPACE)) {  // シーン変遷の条件を書く
     //    isEnd_ = true;
@@ -59,15 +49,44 @@ void GameScene::Update() {
 }
 
 void GameScene::Draw() {
+    // コマンドリストの取得
+    ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+    // 背景スプライト描画前処理
+    Sprite::PreDraw(commandList);
+
+
+
+    // スプライト描画後処理
+    Sprite::PostDraw();
+
+    // 深度バッファクリア
+    dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
     // 3Dオブジェクト描画前処理
     Model::PreDraw();
 
+    // 天球
     skydome_->Draw();
 
+    // Player
     player_->Draw();
 
     // 3Dオブジェクト描画後処理
     Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+    // 前景スプライト描画前処理
+    Sprite::PreDraw(commandList);
+
+
+    // スプライト描画後処理
+    Sprite::PostDraw();
+#pragma endregion
 }
 
 IScene* GameScene::NextScene() const {
