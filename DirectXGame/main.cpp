695997a30065de6f58cf48f57e48e4f2ff84dc11
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2026-02-20 16:28:40 +0900

    マウスカーソルが見えないように変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -13,12 +13,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
     // KamataEngineの初期化
     KamataEngine::Initialize(L"LE3C_10_タカキ_ケンゴ_AstroWing");
 
+    // ゲームプレイ中はOSカーソルを非表示にする
+    while (ShowCursor(FALSE) >= 0) {}
+
     // DirectXCommonのインスタンスの取得
     DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
     // 各シーンの初期化処理 ===============
     SceneManager sceneManager;
-	sceneManager.SetInitialScene(std::make_unique<TitleScene>());
+    sceneManager.SetInitialScene(std::make_unique<TitleScene>());
 
     //===============
     // Mainループ
@@ -35,7 +38,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
         //=============
 
         sceneManager.Update();
-        
+
         //=============
         /// 描画処理 
         //=============
@@ -44,13 +47,16 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
         dxCommon->PreDraw();
 
         sceneManager.Draw();
-                
+
         //描画処理終了
         dxCommon->PostDraw();
     }
 
     // KamataEngineの終了 
     KamataEngine::Finalize();
+
+    // アプリ終了時にOSカーソルを元に戻す
+    while (ShowCursor(TRUE) < 0) {}
 
     return 0;
 }
